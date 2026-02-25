2797457698cbad94e21a3e8c1935332ebbfd84a0
Eric Chen <echen9870@gmail.com>
2026-02-24 20:56:57 -0500

    Revamped Testing framework

--- Main.java
+++ Main.java
@@ -11,94 +11,67 @@ public class Main {
 
         // Build framework and generate numbers
         TestingFramework framework = new TestingFramework(universe);
-        framework.generateNumbers(1 << 24);
+        framework.generateOps(1 << 28, 1 << 20);
+
+        
 
         List<RunResult> results = new ArrayList<>();
 
-
-        // XFast
-        {
-            XFastTree xfast = new XFastTree(bits);
-            RunResult r = framework.run(
-                    "XFastTree",
-                    1,
-                    (x) -> xfast.insert(x),
-                    (x) -> xfast.query(x),
-                    (x) -> xfast.successor(x)
-            );
-
-            results.add(r);
-        }
-        // Tested on Eric's PC 1 -> 8 thread leads to half the time for insertion queries, but everything else lags behind
-        // XFast 4 threads
-        // {
-        //     RunResult r = framework.run(
-        //             "XFastTree",
-        //             4,
-        //             (x) -> xfast.insert(x),
-        //             (x) -> xfast.query(x),
-        //             (x) -> xfast.successor(x)
-        //     );
-
-        //     results.add(r);
-        // }
-
-        {
-            YFastTree yFastTree = new YFastTree(bits);
-            RunResult r = framework.run(
-                    "YFastTree",
-                    1,
-                    (x) -> yFastTree.insert(x),
-                    (x) -> yFastTree.query(x),
-                    (x) -> yFastTree.successor(x)
-            );
-
-            results.add(r);
-        }
-
         // BST
+        TreeSet<Integer> bst = new TreeSet<>();
         {
-            TreeSet<Integer> set = new TreeSet<>();
-            RunResult r = framework.run(
+            RunResult r = framework.runRandom(
                     "BST",
                     1,
-                    (x) -> set.add(x),
-                    (x) -> set.contains(x),
-                    (x) -> set.ceiling(x)
+                    (x) -> bst.add(x),
+                    (x) -> bst.contains(x),
+                    (x) -> bst.ceiling(x)
             );
 
             results.add(r);
         }
 
-        // ConcurrentSkipListSet 1 thread
-        // {
-        //     ConcurrentSkipListSet<Integer> set = new ConcurrentSkipListSet<>();
+        // XFast
+        XFastTree xFast = new XFastTree(bits);
+        {
+            RunResult r = framework.runRandom(
+                    "XFastTree",
+                    1,
+                    (x) -> xFast.insert(x),
+                    (x) -> xFast.query(x),
+                    (x) -> xFast.successor(x)
+            );
 
-        //     RunResult r = framework.run(
-        //             "ConcurrentSkipListSet",
-        //             1,
-        //             (x) -> set.add(x),
-        //             (x) -> set.contains(x),
-        //             (x) -> set.ceiling(x)
-        //     );
+            results.add(r);
+        }
 
-        //     results.add(r);
-        // }
+        // YFast
+        YFastTree yFast = new YFastTree(bits);
+        {
+            RunResult r = framework.runRandom(
+                    "YFastTree",
+                    1,
+                    (x) -> yFast.insert(x),
+                    (x) -> yFast.query(x),
+                    (x) -> yFast.successor(x)
+            );
 
-        // ConcurrentSkipListSet 4 threads
-        // {
-        //     ConcurrentSkipListSet<Integer> set = new ConcurrentSkipListSet<>();
+            results.add(r);
+        }
 
-        //     RunResult r = framework.run(
-        //             "ConcurrentSkipListSet",
-        //             4,
-        //             (x) -> set.add(x),
-        //             (x) -> set.contains(x),
-        //             (x) -> set.ceiling(x)
-        //     );
+        // Test Correctness
+        for (int x = 0; x < universe; x++) {
+            boolean b0 = bst.contains(x);
+            boolean b1 = xFast.query(x);
+            boolean b2 = yFast.query(x);
 
-        //     results.add(r);
-        // }
+            if (b0 != b1) {
+                throw new RuntimeException("Existence mismatch (XFAST) at x=" + x + " bst=" + b0 + " xfast=" + b1);
+            }
+            if (b0 != b2) {
+                throw new RuntimeException("Existence mismatch (YFAST) at x=" + x + " bst=" + b0 + " yfast=" + b2);
+            }
+        }
 
         // Print results
         for (RunResult r : results) {
